af45e1f5dd2d2ad7221e9d4412d8fd2161997a88
Vijayakumar <105634584+mrvkr@users.noreply.github.com>
2022-05-18 12:56:35 +0530

    Update streamlit_app.py
    added index

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,9 +9,13 @@ streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 streamlit.text('🍌🍋Build Your Own Smoothie 🥝🍇')
 
+
 import pandas 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
 # Let's put pick list
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
 #Display the table
 streamlit.dataframe(my_fruit_list)
