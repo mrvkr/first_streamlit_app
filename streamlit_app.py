0c27739a3e9f25a7d1d0cef21ea16b9a066fba5e
Vijayakumar <105634584+mrvkr@users.noreply.github.com>
2022-05-20 12:23:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,14 +59,14 @@ if streamlit.button('Get Fruit Load List'):
   streamlit.dataframe(my_data_rows)
 #Allow end user to add a fruit to list
 def insert_row_snowflake(new_fruit):
-    with my_cnx.curson() as my_cur:
-        my_cur.execute("insert into fruit_load_list values ('from streamlit')")  
-        return "Thanks for adding " + new_fruit
+  with my_cnx.curson() as my_cur:
+    my_cur.execute("insert into fruit_load_list values ('jackfruit')")  
+    return "Thanks for adding " + new_fruit
   
-    add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-    if streamlit.button('Add a Fruit to the List'):
-        my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-        back_from_function = insert_row_snowflake(add_my_fruit)
-        streamlit.text(back_from_function)
-        my_cnx.close()
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_function)
+  my_cnx.close()
 #streamlit.write('Thanks for adding', add_my_fruit)
